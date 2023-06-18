219b1e46a15ae585bbc84c5cf01d4a5e8151cc7f
Keerthana KJ <87189032+keerthanakj@users.noreply.github.com>
2023-06-18 17:05:13 +0530

    Add files via upload

---
+++ BooleanCheck.java
@@ -0,0 +1,13 @@
+package myPkg;
+
+public class BooleanCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10,j=20;
+		boolean k;
+		k=j>i;
+		System.out.println(k);
+	}
+
+}
